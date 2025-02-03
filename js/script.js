d11e35b6bc3b9b02021b40dd003c77d6d8088001
Marine <marinekislova@gmail.com>
2025-02-03 13:23:20 +0200

    2025/02/03

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const cardFont = document.querySelector(".front");
+const cardBack = document.querySelector(".back");
+console.log(cardItem);
+
+function flipCard() {
+  function onCardClick() {
+
+  }
+  cardFont.addEventListener("click", onCardMove);
+}
